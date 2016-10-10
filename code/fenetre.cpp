300b946623e647ffd19af0f2edd653eca1a6f2ea
MILLET <Rudymil@users.noreply.github.com>
2016-10-10 09:43:41 +0200

    0.2.1
    window redevient fenetre.
    Remise en forme.

--- code/fenetre.cpp
+++ code/fenetre.cpp
@@ -2,7 +2,7 @@
 
 #include "fenetre.h"
 
-window::fenetre(QWidget *owner)
+fenetre::fenetre(QWidget *owner)
     :QWidget{owner},
     titre{new QLabel{"",this}}, // construction du titre
     reel{new QLineEdit{"",this}}, // construction du champs d entree
@@ -10,31 +10,31 @@ window::fenetre(QWidget *owner)
     resultat{new QLabel{"",this}} // construction de la sortie
     {
 
-    // construction du layout vertical
+// construction du layout vertical
     auto mise_en_forme_V = new QVBoxLayout{}; // le layout, il ne sert que dans le constructeur, donc variable locale du constructeur, pas variable membre de la classe
 
-    // personnalisation de la fenetre
+// personnalisation de la fenetre
     setFixedSize(350,200);
     setWindowIcon(QIcon("icone.png"));
     setWindowTitle("Norme IEEE 754");
 
-    // personalisation du titre
+// personalisation du titre
     titre->setText("Saisissez un nombre réel puis convertissez-le en\nsimple précision (32 bits) conformément à la\nnorme IEEE 754");
     titre->setFont(QFont("Helvetica", 11));
     titre->setAlignment(Qt::AlignCenter);
 
-    // personalisation du reel
+// personalisation du reel
     //reel->maxLength();
     reel->setAlignment(Qt::AlignCenter);
     reel->setFont(QFont("Helvetica", 11));
 
-    // personnalisation du bouton
+// personnalisation du bouton
     conversion->setText("Conversion"); // que nous appelons tout simplement "Conversion"
     conversion->setFont(QFont("Helvetica", 11));
     conversion->setToolTip("Convertit et affiche sous forme d'une chaîne de caractères représentant les bits (0,1)");
     //conversion->move(60, 50);
 
-    // ajout des widgets
+// ajout des widgets
     mise_en_forme_V->addWidget(titre);
     mise_en_forme_V->addWidget(reel);
     mise_en_forme_V->addWidget(conversion);
