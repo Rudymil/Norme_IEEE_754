f4f89eef4f3be6b3253de1617d22bd95bc9acd10
MILLET <Rudymil@users.noreply.github.com>
2016-10-10 09:39:22 +0200

    0.2.1
    Remise en forme.

--- code/fenetre.h
+++ code/fenetre.h
@@ -12,30 +12,21 @@
 
 class fenetre : public QWidget{ // On herite de QWidget (IMPORTANT)
 
-
         Q_OBJECT // obligatoire pour utiliser les signaux et les slots
-
 
     public:
         fenetre(QWidget * owner = nullptr); // on doit toujours pouvoir passer un parent a un QWidget, c est comme ça que fonctionne le mecanisme de gestion de la memoire de Qt.
 
-
-
     public slots:
         void onClick();
-
 
     private:
         QLabel *titre; // enonce
         QLabel *resultat; // sortie
-
         QLineEdit *reel; // champs d entree
-
         QPushButton *conversion; // bouton d enclenchement
-
         /* inutile de garder un pointeur sur le layout
         QVBoxLayout *mise_en_forme_V; // mise en forme verticale */
-
 
 };
 
